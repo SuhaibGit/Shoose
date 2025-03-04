f50ac1b278e2c20b8e0fac2fcdb4d7b276616ee4
SuhaibGit <m.suhaib1906@gmail.com>
2025-03-04 22:46:52 +0500

    first commit

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -8,14 +8,16 @@ const Hero = () => {
     <div className="h-[600px] bg-[url('/assets/p1.jpg')] bg-cover bg-center" ></div>
     <div className="h-[450px] bg-[url('/assets/hero.jpg')] bg-cover bg-center" ></div>
     <div className='h-[50px] bg-amber-200'>
-        <div className='flex justify-evenly items-center text-black h-full'>
-            <div className='flex items-center'>
-            <TbTruckDelivery className='mr-2' /><h1>FREE SHIPPING ACCROSS PAKISTAN</h1>
-            </div>
-            <div className='flex items-center'>
-            <IoIosCall className='mr-2' /><h1>CALL US AT +92312345678</h1>
-            </div>
-        </div>
+        <div className="flex flex-wrap justify-center md:justify-evenly items-center text-black h-full px-4 py-2">
+          <div className="flex items-center text-xs sm:text-sm md:text-lg lg:text-xl mb-2 md:mb-0">
+            <TbTruckDelivery className="mr-2 text-base sm:text-lg md:text-xl" />
+            <h1 className="text-center">FREE SHIPPING ACROSS PAKISTAN</h1>
+          </div>
+          <div className="flex items-center text-xs sm:text-sm md:text-lg lg:text-xl">
+            <IoIosCall className="mr-2 text-base sm:text-lg md:text-xl" />
+            <h1 className="text-center">CALL US AT +92312345678</h1>
+          </div>
+    </div>
     </div>
     <div className="flex justify-center text-center bg-white pt-7">
     <h3 className="flex text-xl ">
