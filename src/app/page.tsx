4e246360c94822db5c5c0487e5a065d6db3c03ed
SuhaibGit <m.suhaib1906@gmail.com>
2025-03-04 22:38:28 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Navbar1 from "./components/navbar";
 import Hero from "./components/hero";
 import Listing from "./components/listing";
+import Footer from "./components/footer";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
    <Navbar1/>
    <Hero/>
    <Listing/>
+   <Footer/>
    </>
   );
 }
